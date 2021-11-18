98bc053c95be917b346e9d883faba8fa27661617
Elase Huang <elase.huang@outlook.com>
2021-11-18 13:04:58 +0800

    [webpack] use sass-loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -26,6 +26,15 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ]
+            },
         ],
     },
     plugins: [
