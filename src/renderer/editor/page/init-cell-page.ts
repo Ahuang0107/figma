8e455a038730001bec6b6da1debe338075794c0a
Elase Huang <elase.huang@outlook.com>
2022-11-01 18:14:15 +0800

    temp

--- src/renderer/editor/page/init-cell-page.ts
+++ src/renderer/editor/page/init-cell-page.ts
@@ -3,30 +3,27 @@ import {Rect} from "../base/rect";
 import sk from "../utils/canvas-kit";
 import {SkyLineView} from "../view/line-view";
 import {SkyRectView} from "../view/rect-view";
-import {TimeStamp, TimeStampConverter} from "../utils/time-stamp";
+import {TimeStamp} from "../utils/time-stamp";
 
 export async function initCellView(): Promise<SkyPageView> {
     const pageView = new SkyPageView();
     /*2022-10-31作为显示原点*/
     const origin = 1666659600000;
-    const scaleX = 25;
+    const cellWidth = 25;
     const cellHeight = 20;
-    const cellMargin = 5;
 
     const cellBorderColor = sk.CanvasKit.Color(204, 204, 204);
-    const SUNEZUMI = sk.CanvasKit.Color(120, 125, 123);
-
-    const converter = new TimeStampConverter();
+    const weekendColor = sk.CanvasKit.Color(120, 125, 123);
 
     /*渲染垂直的单元格边框逻辑*/
     const start = TimeStamp.from(Date.UTC(2022, 0, 1));
     const end = TimeStamp.from(Date.UTC(2023, 0, 1));
     start.loop(TimeStamp.DAY_DURING, end, (time) => {
-        const beforeStart = scaleX * (time.milliseconds - origin) / 1000 / 60 / 60 / 24;
+        const beforeStart = cellWidth * (time.milliseconds - origin) / 1000 / 60 / 60 / 24;
         const lineView = new SkyLineView(new Rect(beforeStart, 0, 0, 2000), cellBorderColor);
         pageView.push(lineView);
         if ((time.milliseconds - 144000) % 604800000 == 0) {
-            const rectView = new SkyRectView(new Rect(beforeStart, 0, scaleX * 2, 2000), SUNEZUMI, 0, false);
+            const rectView = new SkyRectView(new Rect(beforeStart, 0, cellWidth * 2, 2000), weekendColor, 0, false);
             pageView.push(rectView);
         }
     })
@@ -34,7 +31,7 @@ export async function initCellView(): Promise<SkyPageView> {
     /*渲染水平的单元格边框逻辑*/
     let index = 0;
     while (index < 100) {
-        const y = index * (cellHeight + cellMargin);
+        const y = index * cellHeight;
         const lineView = new SkyLineView(new Rect(-8000, y, 20000, 0), cellBorderColor);
         pageView.push(lineView);
         index++;
